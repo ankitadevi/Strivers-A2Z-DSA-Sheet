eca5339bed8ac49768bd36ab0e31379c59ae32a3
ANKITA DEVI <ad2004ankita@gmail.com>
2026-05-20 11:45:26 +0530

    Update 05. Postorder Traversal.cpp

--- 11. Binary Trees/1. Traversals/05. Postorder Traversal.cpp
+++ 11. Binary Trees/1. Traversals/05. Postorder Traversal.cpp
@@ -13,17 +13,18 @@ Complexity Analysis:
 Code:
 */
 
-vector<int> postorderTraversal(TreeNode* root) {
-    vector<int> ans;
-    solve(root, ans);
-    return ans;
-}
-
-void solve(TreeNode* root, vector<int>& ans) {
-    if (!root) {
-        return;
+class Solution {
+public:
+    void solve(TreeNode* root,vector<int>& ans){
+        if(!root) return;
+        solve(root->left,ans);
+        solve(root->right,ans);
+        ans.push_back(root->val);
     }
-    solve(root->left, ans);
-    ans.push_back(root->val);
-    solve(root->right, ans);
-}
+    
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        solve(root,ans);
+        return ans;
+    }
+};
